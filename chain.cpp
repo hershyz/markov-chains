681dee25789c80d2b307138c5ab300c5b148e216
Hrishikesh Bagalkote <hbagalkote@gmail.com>
2024-11-12 23:07:15 -0500

    renames

---
+++ chain.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// organization for markov chains/transition matrices
+class Chain {
+public:
+
+    // parse historical csv
+    void parse(string csv_path) {
+        
+    }
+};
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // instance-level variables and parameters
-string data_path = "historical.csv";
+string csv_path = "historical.csv";
 double negative_momentum_threshold = -3.50;         // (-inf, negative_momentum_threshold) = negative momentum
 double positive_momentum_threshold = 3.50;          // (positive_momentum_threshold, inf) = positive momentum
                                                     // [negative_momentum_threshold, positive_momentum_threshold] = neutral momentum
