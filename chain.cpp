3429e92a76365b795be053ff511f2385f6adffef
Hrishikesh Bagalkote <hbagalkote@gmail.com>
2024-11-12 23:45:56 -0500

    parsing...

--- chain.cpp
+++ chain.cpp
@@ -7,7 +7,11 @@ using namespace std;
 class Chain {
 public:
 
+    // asset list for numerical indexing (avoiding hash functions for as long as possible)
     vector<string> asset_list;
+
+    // price matrix (price_mat[asset index][day])
+    vector<vector<int> > price_mat;
 
     // parse historical csv
     void parse(string csv_path) {
@@ -18,5 +22,12 @@ public:
         getline(file, first);
         asset_list = split(first, ',');
         asset_list.erase(asset_list.begin());
+
+        // populate price matrix
+        price_mat.resize(asset_list.size());
+        string raw_price_string;
+        while (getline(file, raw_price_string)) {
+            cout << raw_price_string << endl;
+        }
     }
 };
