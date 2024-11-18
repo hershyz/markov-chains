c1184315b45543ac317a7bb5e473aed5317c743d
Hrishikesh Bagalkote <hbagalkote@gmail.com>
2024-11-17 20:00:52 -0500

    add double split util function, parse into price matrix, update historical csv

--- chain.cpp
+++ chain.cpp
@@ -7,6 +7,10 @@ using namespace std;
 class Chain {
 public:
 
+    // split ratios for training and backtest (80-20 by default)
+    double training_split = 0.8;
+    double backtest_split = 0.2;
+
     // asset list for numerical indexing (avoiding hash functions for as long as possible)
     vector<string> asset_list;
 
@@ -27,7 +31,15 @@ public:
         price_mat.resize(asset_list.size());
         string raw_price_string;
         while (getline(file, raw_price_string)) {
-            cout << raw_price_string << endl;
+            vector<double> price_row = split_double(raw_price_string, ',');
+            price_row.erase(price_row.begin());
+            for (size_t i = 0; i < price_row.size(); i++) {
+                price_mat[i].push_back(price_row[i]);
+            }
+        }
+
+        for (size_t i = 0; i < price_mat.size(); i++) {
+            cout << price_mat[i].size() << endl;
         }
     }
 };
--- util.cpp
+++ util.cpp
@@ -13,3 +13,18 @@ vector<string> split(const string& str, char token) {
     }
     return res;
 }
+
+// split into doubles by the token character
+vector<double> split_double(const string& str, char token) {
+    vector<double> res;
+    stringstream ss(str);
+    string curr;
+    while (getline(ss, curr, token)) {
+        try {
+            res.push_back(stod(curr));
+        } catch (const invalid_argument e) {
+            continue;
+        }
+    }
+    return res;
+}
